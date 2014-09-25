5cda16e1f5f72124faa0bbcd82da3ba608927021
Laura Trigeiro <lauratrigeiro@gmail.com>
2014-09-25 16:02:43 -0700

    fixed no more blanks inputs

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-	validates FILL_IN, presence: true
-  	validates FILL_IN, presence: true
+	validates :name, presence: true
+  	validates :email, presence: true
 	has_many :microposts
 end
